178bb252ac0179932ba60dde77478c94e3a9b974
FelipeDib <felipedibcastro@gmail.com>
2025-01-28 23:12:51 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrentyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrentyException : ApplicationException
+    {
+        public DbConcurrentyException(string message) : base(message)
+        {
+             
+        }
+    }
+}
