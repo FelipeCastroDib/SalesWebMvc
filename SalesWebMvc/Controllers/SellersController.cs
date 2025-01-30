eada4a84001cc78432a84df18502628c5cf5f729
FelipeDib <felipedibcastro@gmail.com>
2025-01-29 22:00:47 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -35,8 +35,14 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
        public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid) 
+            {
+                var departaments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departaments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Delete(int? id) 
@@ -99,6 +105,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departaments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departaments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
